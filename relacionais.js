13bb48e37a23edc9a09d201096cb0524a19147ab
Júlia Pereira <jutella31@gmail.com>
2025-01-29 10:08:29 -0300

    Initial commit

---
+++ relacionais.js
@@ -0,0 +1,60 @@
+console.log("atv1")
+let a = 5
+let b = 9
+
+let resultado = a > b
+let resultado2 = a >= b 
+let resultado3 = a == b
+
+console.log(resultado)
+console.log(resultado2)
+console.log(resultado3)
+
+//-----------------------------------------
+console.log("atv2")
+let idade = 17
+
+let maiorIdade = idade >= 16
+let obrigadoVotar = idade >= 18
+let obrigadoVotar2 = idade <= 70
+
+console.log(maiorIdade)
+console.log(obrigadoVotar)
+console.log(obrigadoVotar2)
+
+//-----------------------------------------
+console.log("atv3")
+let idade2 = 52
+
+let maior = idade2 >= 18
+let idoso = idade2 >= 60
+
+console.log(maior)
+console.log(idoso)
+
+//-----------------------------------------
+console.log("atv4")
+let prog = "admin" 
+
+let usuario = prog == "admin"
+
+console.log(usuario)
+//-----------------------------------------
+console.log("atv5")
+let c = 4
+
+let div = 4 % 3
+let divi = 4 % 5
+
+console.log(div)
+console.log(divi)
+//-----------------------------------------
+console.log("atv6")
+let conta = 500
+let saque = 200
+
+let suficiente = conta - saque
+let situacao = suficiente > 0
+
+console.log(suficiente)
+console.log(situacao)
